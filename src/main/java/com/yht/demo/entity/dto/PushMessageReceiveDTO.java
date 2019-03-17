ecb4757e685b43894ae9795b190299126d8d2ef2
yht <930237972@qq.com>
2019-03-17 13:59:57 +0800

    22

--- src/main/java/com/yht/demo/entity/dto/PushMessageReceiveDTO.java
+++ src/main/java/com/yht/demo/entity/dto/PushMessageReceiveDTO.java
@@ -2,10 +2,12 @@ package com.yht.demo.entity.dto;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
 
 import java.util.List;
 
 @ApiModel(value="推送参数")
+@Data
 public class PushMessageReceiveDTO {
 
     @ApiModelProperty(name = "title", value = "推送标题", required = true)
@@ -18,41 +20,5 @@ public class PushMessageReceiveDTO {
     private String productUrl;
     @ApiModelProperty(name = "clientNames", value = "多个客户端名称，逗号隔开", required = true)
     private String clientNames;
-
-
-    public String getTitle() {
-        return title;
-    }
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public String getIcon() {
-        return icon;
-    }
-    public void setIcon(String icon) {
-        this.icon= icon;
-    }
-
-    public String getProductUrl() {
-        return productUrl;
-    }
-    public void setProductUrl(String productUrl) {
-        this.productUrl= productUrl;
-    }
-
-    public String getClientNames() {
-        return clientNames;
-    }
-    public void setClientNames(String clientNames) {
-        this.clientNames= clientNames;
-    }
 
 }
