bdec7dc0b3be97206b492f54ca26ac710ef2b8ed
yht <930237972@qq.com>
2019-03-23 19:39:02 +0800

    22

--- src/main/java/com/yht/demo/service/impl/UvStatisticsServiceImpl.java
+++ src/main/java/com/yht/demo/service/impl/UvStatisticsServiceImpl.java
@@ -5,13 +5,16 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.yht.demo.common.BaseService;
 import com.yht.demo.common.Result;
 import com.yht.demo.entity.dto.UvStatisticsReceiveDTO;
-import com.yht.demo.entity.model.UserManager;
 import com.yht.demo.mapper.UserMapper;
 import com.yht.demo.mapper.UvStatisticsMapper;
 import com.yht.demo.service.IUvStatisticsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Map;
 
 @Service
@@ -28,9 +31,22 @@ public class UvStatisticsServiceImpl extends BaseService implements IUvStatistic
         Page page = new Page();
         page.setSize(uvStatisticsReceiveDTO.getPageSize());
         page.setCurrent(uvStatisticsReceiveDTO.getPageNum());
+        if (!StringUtils.isEmpty(uvStatisticsReceiveDTO.getEndTime())){
+            try {
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+                Date sDate = sdf.parse(uvStatisticsReceiveDTO.getEndTime());
+                Calendar c = Calendar.getInstance();
+                c.setTime(sDate);
+                c.add(Calendar.DAY_OF_MONTH, 1);
+                sDate = c.getTime();
+                SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy-MM-dd");
+                uvStatisticsReceiveDTO.setEndTime(sdf1.format(sDate));
+            }catch (Exception e){
+                log.error("日期加一天========" + e.getMessage());
+            }
+        }
         //uv点击数
         IPage<Map> list = uvStatisticsMapper.getUVClickCount(page, uvStatisticsReceiveDTO);
-
         return Result.success(list);
     }
 
